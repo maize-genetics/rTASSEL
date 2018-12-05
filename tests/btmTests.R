175e766b782939c8cb51ff17e13d96e4224f35d2
Brandon Monier <brandon.monier@gmail.com>
2018-12-05 16:03:53 -0500

    Add paths

--- tests/btmTests.R
+++ tests/btmTests.R
@@ -14,8 +14,31 @@
 #    capabilities for rTASSEL in generating GenomicRanges class
 #--------------------------------------------------------------------
 
+## Source files
 source("R/AllClasses.R")
 source("R/AllGenerics.R")
 source("R/TasselPluginWrappers.R")
+
+
+## jinit
+rJava::.jinit()
+
+## Add class path
+homeloc <- Sys.getenv("HOME")
+rJava::.jaddClassPath(
+    paste0(homeloc, "/Development/tassel_5_standalone/lib")
+)
+rJava::.jaddClassPath(
+    paste0(homeloc, "/Development/tassel_5_standalone/sTASSEL.jar")
+)
+
+
+## Add VCF path
+devloc <- paste0(homeloc, "/Development")
+vcfPath <- paste0(
+    devloc,
+    "/tassel_5_test/dataFiles/GenotypeTableTests/",
+    "maize_chr9_10thin100.recode.vcf"
+)
 
 
