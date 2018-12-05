92340964fb472f9f3b5f0329c072de6dc600e044
Brandon Monier <brandon.monier@gmail.com>
2018-12-05 16:02:47 -0500

    Initial commit

---
+++ tests/btmTests.R
@@ -0,0 +1,21 @@
+#!/usr/bin/env Rscript
+
+#--------------------------------------------------------------------
+# Script Name:   btmTests.R
+# Description:   GenomicRanges test for rTASSEL
+# Author:        Brandon Monier
+# Created:       2018-12-04 at 16:46:06
+# Last Modified: 2018-12-04 at 19:00:38
+#--------------------------------------------------------------------
+
+#--------------------------------------------------------------------
+# Detailed Purpose:
+#    The main purpose of this Rscript is to test out wrapper
+#    capabilities for rTASSEL in generating GenomicRanges class
+#--------------------------------------------------------------------
+
+source("R/AllClasses.R")
+source("R/AllGenerics.R")
+source("R/TasselPluginWrappers.R")
+
+
