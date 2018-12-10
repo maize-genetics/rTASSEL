d35041a38bff0cee4135d52030fb9e7120a76c96
Edward Buckler <esb33@cornell.edu>
2018-12-09 21:32:08 -0500

    Working calls to passing GenotypingTable by vectors

--- tests/initialTests.R
+++ tests/initialTests.R
@@ -1,12 +1,53 @@
 #Test script for R classes and functions
 
-setwd("/Users/edwardbuckler/Code/rtassel/")
-setwd()
+##Installs needed
+# install.packages("tidyverse")
+# install.packages("rJava", dependencies=TRUE, type="source")
+# if (!requireNamespace("BiocManager")) {
+#   install.packages("BiocManager")
+# }
+# BiocManager::install("SummarizedExperiment")
 
-#compiles the classes needed
+## Load packages
+library(rJava)
+library(GenomicRanges)
+library(stringr)
+
+
+is_experimental <- TRUE
+path_exp_tassel <- "/Users/edwardbuckler/Code/tassel-5-source/dist/sTASSEL.jar"
+
+## Set WD
+setwd("~/Code/rtassel")
+
+## jinit
+rJava::.jinit()
+.jcall(.jnew("java/lang/Runtime"), "J", "totalMemory")
+.jcall(.jnew("java/lang/Runtime"), "J", "maxMemory")
+
+## Add class path
+# Note the file class paths may differ between Windows and Macs.
+homeloc <- Sys.getenv("HOME")
+if(is_experimental == TRUE) {
+  tasselPath <- path_exp_tassel
+} else {
+  tasselPath <- paste0(homeloc, "/Code/tassel-5-standalone/sTASSEL.jar")
+}
+
+rJava::.jaddClassPath(tasselPath)
+print(.jclassPath())
+
+tasselVersion <- rJava::.jfield("net/maizegenetics/tassel/TASSELMainFrame","S","version")
+str_c("Using TASSEL version: ",tasselVersion)
+
+# rJava::.jaddClassPath("/Users/edwardbuckler/Code/tassel-5-source/dist/sTASSEL.jar")
+
+## Source files
 source("R/AllGenerics.R")
 source("R/AllClasses.R")
 source("R/TasselPluginWrappers.R")
+source("R/PullFunctions.R")
+
 
 ## VCF file path exmaple...
 vcfPath <- paste0(
@@ -28,3 +69,25 @@ testTaxa <- taxa(test)
 testTaxa
 
 sampleDF <- sampleDataFrame(testTaxa)
+
+genoIntArray <- rJava::.jcall(
+  "net/maizegenetics/plugindef/GenerateRCode",
+  "[I",
+  "genotypeTableToDosageIntArray",
+  test@jtsGenotypeTable
+)
+
+genoNameArray <- rJava::.jcall(
+  "net/maizegenetics/plugindef/GenerateRCode",
+  "[S",
+  "genotypeTableToSampleNameArray",
+  test@jtsGenotypeTable
+)
+
+genoPositionVector <- rJava::.jcall(
+  "net/maizegenetics/plugindef/GenerateRCode",
+  "Lnet/maizegenetics/plugindef/GenerateRCode$PositionVectors;",
+  "genotypeTableToPositionListOfArrays",
+  test@jtsGenotypeTable
+)
+
