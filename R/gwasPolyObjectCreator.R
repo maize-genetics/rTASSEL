06b2a8ef6278697125dacc6105d3f8e2539a6589
Unknown <arcadio.valdes@gmail.com>
2018-12-11 14:40:00 -0500

    function to create GWASpoly object from summarizedExperiment
    and added example tassel  hmp genotypes and phenotypes

---
+++ R/gwasPolyObjectCreator.R
@@ -0,0 +1,118 @@
+# Coopting read.gwaspoly to create GWASpoly class object from tassel simplified experiment
+#right now it makes dummy marker names as current summarizeExperimentFromGenotypeTable doesn't keep them
+se_createGWASpolyObject <- function(ploidy, phenoDF, simplifiedExperimentGeno, format, n.traits)
+{
+  if (format == "ACTG") {
+    format <- "ACGT"
+  }
+  if (!is.element(format, c("AB", "numeric", "ACGT"))) {
+    stop("Invalid genotype format.")
+  }
+  bases <- c("A", "C", "G", "T")
+  get.ref <- function(x, format) {
+    if (format == "numeric") {
+      ref.alt <- c(0, 1)
+    }
+    if (format == "AB") {
+      ref.alt <- c("A", "B")
+    }
+    if (format == "ACGT") {
+      y <- paste(na.omit(x), collapse = "")
+      ans <- apply(array(bases), 1, function(z, y) {
+        length(grep(z, y, fixed = T))
+      }, y)
+      if (sum(ans) > 2) {
+        stop("Error in genotype matrix: More than 2 alleles")
+      }
+      if (sum(ans) == 2) {
+        ref.alt <- bases[which(ans == 1)]
+      }
+      if (sum(ans) == 1) {
+        ref.alt <- c(bases[which(ans == 1)], NA)
+      }
+    }
+    return(ref.alt)
+  }
+
+  ## unwrap tassel summarized experiment to expected GWASpoly genotype format
+  geno <- data.frame(markerName = paste("dummy", 1:length(ranges(simplifiedExperimentGeno@rowRanges))), # dummy name as current summarizeExperimentFromGenotypeTable doesn't keep
+                     chr = seqnames(simplifiedExperimentGeno@rowRanges),
+                     pos = start(ranges(simplifiedExperimentGeno@rowRanges)),
+                     as.data.frame(simplifiedExperimentGeno@assays$data@listData) # same as assay(simplifiedExperimentGeno)
+  )
+  colnames(geno)[4:ncol(geno)] <- as.character(simplifiedExperimentGeno$Sample)
+
+  map <- data.frame(Marker = geno[, 1], Chrom = factor(geno[,
+                                                            2], ordered = T), Position = geno[, 3], stringsAsFactors = F)
+  markers <- as.matrix(geno[, -(1:3)])
+  rownames(markers) <- geno[, 1]
+  tmp <- apply(markers, 1, get.ref, format)
+  map$Ref <- tmp[1, ]
+  map$Alt <- tmp[2, ]
+  if (is.element(format, c("AB", "ACGT"))) {
+    M <- apply(cbind(map$Ref, markers), 1, function(x) {
+      y <- gregexpr(pattern = x[1], text = x[-1], fixed = T)
+      ans <- as.integer(lapply(y, function(z) {
+        ifelse(z[1] < 0, ploidy, ploidy - length(z))
+      }))
+      return(ans)
+    })
+  }
+  else {
+    M <- t(markers)
+  }
+  gid.geno <- colnames(geno)[-(1:3)]
+  rownames(M) <- gid.geno
+  bad <- length(which(!is.element(na.omit(M), 0:ploidy)))
+  if (bad > 0) {
+    stop("Invalid marker calls.")
+  }
+  MAF <- apply(M, 2, function(x) {
+    AF <- mean(x, na.rm = T)/ploidy
+    MAF <- ifelse(AF > 0.5, 1 - AF, AF)
+  })
+  polymorphic <- which(MAF > 0)
+  M <- M[, polymorphic]
+  map <- map[polymorphic, ]
+  map <- map[order(map$Chrom, map$Position), ]
+  M <- M[, map$Marker]
+  m <- nrow(map)
+  cat(paste("Number of polymorphic markers:", m, "\n"))
+  impute.mode <- function(x) {
+    ix <- which(is.na(x))
+    if (length(ix) > 0) {
+      x[ix] <- as.integer(names(which.max(table(x))))
+    }
+    return(x)
+  }
+  missing <- which(is.na(M))
+  if (length(missing) > 0) {
+    cat("Missing marker data imputed with population mode \n")
+    M <- apply(M, 2, impute.mode)
+  }
+  pheno <- phenoDF
+  gid.pheno <- unique(pheno[, 1])
+  gid <- intersect(gid.pheno, gid.geno)
+  pheno <- pheno[is.element(pheno[, 1], gid), ]
+  M <- M[gid, ]
+  N <- length(gid)
+  cat(paste("N =", N, "individuals with phenotypic and genotypic information \n"))
+  n.fixed <- ncol(pheno) - n.traits - 1
+  if (n.fixed > 0) {
+    fixed <- data.frame(pheno[, (n.traits + 2):ncol(pheno)],
+                        stringsAsFactors = F)
+    fixed.names <- colnames(pheno)[(n.traits + 2):ncol(pheno)]
+    colnames(fixed) <- fixed.names
+    pheno <- data.frame(pheno[, 1:(1 + n.traits)], stringsAsFactors = F)
+    cat(paste("Detected following fixed effects:\n", paste(fixed.names,
+                                                           collapse = "\n"), "\n", sep = ""))
+  }
+  else {
+    fixed <- data.frame(NULL)
+  }
+  traits <- colnames(pheno)[-1]
+  cat(paste("Detected following traits:\n", paste(traits, collapse = "\n"),
+            "\n", sep = ""))
+  return(new("GWASpoly", map = map, pheno = pheno, fixed = fixed,
+             geno = M, ploidy = ploidy))
+}
---
+++ tests/arcadioTests.R
@@ -0,0 +1,86 @@
+# Test script for creating SummarizedExperimemt as per
+# https://bioconductor.org/packages/release/bioc/vignettes/SummarizedExperiment/inst/doc/SummarizedExperiment.html
+
+# extends original initialTests.R and btmTests.R of converting tassel GenotypeTable to GenomicRanges
+
+library(rJava)
+library(GenomicRanges)
+library(SummarizedExperiment)
+library(GWASpoly)
+
+is_experimental <- TRUE
+
+#set workdir for rtassel
+setwd("~/myBins/bucklerlabBitbucket/rtassel/")
+
+path_exp_tassel <- paste0(getwd(),"/inst/java/sTASSEL.jar")
+path_exp_tassel_libs <- paste0(getwd(),"/inst/java/lib")
+
+## jinit
+rJava::.jinit(parameters="-Xmx6g")
+.jcall(.jnew("java/lang/Runtime"), "J", "totalMemory")
+.jcall(.jnew("java/lang/Runtime"), "J", "maxMemory")
+
+## Add class paths
+if(is_experimental == TRUE) {
+  tasselPath <- path_exp_tassel
+  tasselLibs <- path_exp_tassel_libs
+}
+
+rJava::.jaddClassPath(tasselPath)
+rJava::.jaddClassPath(tasselLibs)
+print(.jclassPath())
+
+
+## Source files
+source("R/AllGenerics.R")
+source("R/AllClasses.R")
+source("R/TasselPluginWrappers.R")
+source("R/PullFunctions.R")
+source("R/gwasPolyObjectCreator.R")
+
+## Add VCF path
+vcfPath <- paste0(
+  getwd(),
+  "/data/maize_chr9_10thin40000.recode.vcf"
+)
+
+
+# Tests
+
+## Make genotype table from tasses sample data
+tasGenoTable <- readGenotypeTable(paste0(getwd(), "/data/mdp_genotype.hmp.txt"))
+
+## Make summarized experiment from genotypetable
+tas_se <- summarizeExperimentFromGenotypeTable(tasGenoTable) # not working right now, but
+
+## Load phenotype data
+###straight load as dataframe, skpping first two rows on tassel specific phenotype table format
+phenos <- read.table(file = paste0(getwd(), "/data/mdp_phenotype.txt"), skip = 2, header = T, sep = "\t", na.strings = "NaN")
+
+### select sinlge location, as GWASpoly requires single entries for taxa.
+phenosOneLoc <- phenos[phenos$location == "A",]
+rownames(phenosOneLoc) <- phenosOneLoc$Taxa
+###remove location as it is now redundant. Also, GWASpoly expects all traits as initial columns, and fixed effect covariates last
+phenosOneLoc <- phenosOneLoc[,-c(2)]
+
+## create GWASpoly object with coopted read.GWASpoly function
+data_gwasPoly <- se_createGWASpolyObject(ploidy = 2, phenoDF = phenosOneLoc, simplifiedExperimentGeno =  tas_se, format = "numeric", n.traits = 3)
+
+## add kinship information to object
+data_gwasPoly <- set.K(data_gwasPoly)
+
+## set parameters for mixed model
+params <- set.params(fixed=unlist(strsplit("Q1,Q2,Q3", ",")),
+                     fixed.type=rep("numeric",3))
+
+## run gwas
+data_gwasPoly_res <- GWASpoly(data = data_gwasPoly, models = "additive", params = params)
+
+qq.plot(data_gwasPoly_res, trait = "EarDia", model = "additive")
+
+data_gwasPoly_res <- set.threshold(data_gwasPoly_res, method = "FDR", level=0.05)
+
+get.QTL(data = data_gwasPoly_res)
+
+manhattan.plot(data_gwasPoly_res, trait = "EarDia", model = "additive")
