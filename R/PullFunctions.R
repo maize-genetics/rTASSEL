6d30f48b3b42fa7b4f283e122cb5d78780bdc4c0
Edward Buckler <esb33@cornell.edu>
2018-12-12 15:12:33 -0500

    Efficient vector passing to make data frame of taxalist and positionlist

--- R/PullFunctions.R
+++ R/PullFunctions.R
@@ -28,54 +28,37 @@ sampleDataFrame <- function(jtsGenoTableOrTaxaList) {
   } else {
     stop("Object is not of \"TaxaList\" class")
   }
+
+  taxaArray <- J("net/maizegenetics/plugindef/GenerateRCode")$genotypeTableToSampleNameArray(jtsTL)
   
-  #This could be faster
-  # taxaArray <- rJava::.jcall(
-  #   "net/maizegenetics/plugindef/GenerateRCode",
-  #   "[S",
-  #   "genotypeTableToSampleNameArray",
-  #   test@jtsGenotypeTable
-  # )
-  taxaArray <- c()
-  for(i in 1:jtsTL$size()) {
-    #why do I have to do -1L
-    taxaArray[i] = toString(jtsTL$taxaName(i-1L))
-  }
-  colData <- data.frame(Sample=taxaArray, row.names = taxaArray)
-  colData
+  fourNewCols <- str_split(taxaArray,":")
+  colData <- data.frame(Sample=taxaArray, TasselIndex = 0:(length(taxaArray)-1L), row.names = taxaArray,
+                        matrix(unlist(fourNewCols), nrow = length(fourNewCols), byrow=T))
 }
 
 
 ## Constructor for GRanges (GenomicRanges) class object
 genomicRanges <- function(genoTable) {
-    if(is(genoTable,"GenotypeTable")) {
+   if(is(genoTable,"GenotypeTable")) {
         jtsPL <- positions(genoTable)@jtsPositionList
     } else if(genoTable %instanceof% "net.maizegenetics.dna.snp.GenotypeTable") {
       jtsPL <- genoTable$positions()
+    } else if(genoTable %instanceof% "net.maizegenetics.dna.map.PositionList") {
+      jtsPL <- genoTable
     } else {
         stop("Object is not of \"GenotypeTable\" class")
     }
     
-  # genoPositionVector <- rJava::.jcall(
-  #   "net/maizegenetics/plugindef/GenerateRCode",
-  #   "Lnet/maizegenetics/plugindef/GenerateRCode$PositionVectors;",
-  #   "genotypeTableToPositionListOfArrays",
-  #   test@jtsGenotypeTable
-  # )
+    genoPositionVector <- J("net/maizegenetics/plugindef/GenerateRCode")$genotypeTableToPositionListOfArrays(jtsPL)
+    
   
-    numSite <- as.numeric(jtsPL$numberOfSites())
-    physPos <- jtsPL$physicalPositions()
-    
-    cat("Extracting chromosome names for each postion...\n")
-    cat("...is there a quicker way to get this? (~ Brandon)\n")
-    chrName <- lapply(seq_len(numSite), function(pos) {
-        jtsPL$chromosomeName(as.integer(pos - 1))
-    })
-    chrName <- unlist(chrName)
-    
     gr2 <- GenomicRanges::GRanges(
-        seqnames = S4Vectors::Rle(chrName),
-        ranges = IRanges::IRanges(start = physPos, end = physPos)
+      seqnames = S4Vectors::Rle(genoPositionVector$chromosomes),
+      ranges = IRanges::IRanges(start = genoPositionVector$startPos, end = genoPositionVector$startPos),
+      strand = S4Vectors::Rle(genoPositionVector$strand),
+      tasselIndex = 0:(length(genoPositionVector$altAllele)-1L),
+      refAllele = genoPositionVector$refAllele,
+      altAllele = genoPositionVector$altAllele
     )
     return(gr2)
 }
