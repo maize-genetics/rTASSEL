5b8282f2277a14aeed8e2e49bf76dc8f5e216220
Travis Wrightsman <tw493@cornell.edu>
2018-12-17 10:48:37 -0500

    Update static haploplot code

--- tests/testHaploplot.R
+++ tests/testHaploplot.R
@@ -1,30 +1,62 @@
 library(ggplot2)
 library(DBI)
+library(GenomicRanges)
 
 # Parameters
 phg_path <- "/Users/tw493/Downloads/phgMaizeGzip_deltaG_depthAndNonMappedFix.db"
-genic_maize_id_cutoff <- 37804L
-chr <- 2L
-num_ref_ranges_to_plot <- 100L
 # ---
 
 phg_db <- dbConnect(RSQLite::SQLite(), phg_path)
-min_ref_range_id <- as.integer(dbGetQuery(phg_db, 'SELECT MIN(ref_range_id) FROM reference_ranges WHERE chrom == :chr',
-                               params = list(chr = chr)))
-max_ref_range_id <- as.integer(dbGetQuery(phg_db, 'SELECT MAX(ref_range_id) FROM reference_ranges WHERE chrom == :chr AND ref_range_id < :cutoff',
-                               params = list(chr = chr, cutoff = genic_maize_id_cutoff)))
-haps <- dbGetQuery(phg_db, 'SELECT haplotypes.ref_range_id, genotypes.line_name FROM haplotypes INNER JOIN genotypes ON haplotypes.gamete_grp_id=genotypes.genoid WHERE haplotypes.ref_range_id >= :min AND haplotypes.ref_range_id <= :max',
-                   params = list(min = min_ref_range_id, max = max_ref_range_id))
+haps <- dbGetQuery(phg_db, 'SELECT gt.line_name, rr.chrom, rr.range_start, rr.range_end, rr.ref_range_id FROM haplotypes AS ht INNER JOIN genotypes AS gt ON ht.gamete_grp_id=gt.genoid INNER JOIN reference_ranges AS rr ON ht.ref_range_id=rr.ref_range_id')
 dbDisconnect(phg_db)
 
-filtered_haps <- haps[which(haps$ref_range_id < (min_ref_range_id + num_ref_ranges_to_plot)),]
-filtered_haps$ref_range_id <- as.factor(filtered_haps$ref_range_id)
-filtered_haps$line_name <- as.factor(filtered_haps$line_name)
+haps$line_name <- as.factor(haps$line_name)
+haps$chrom <- as.integer(haps$chrom)
+haps$ref_range_id <- as.factor(haps$ref_range_id)
+haps$y <- match(haps$line_name, levels(haps$line_name)) # generate y values for haplotypes based on taxa
+haps$genic <- ifelse(as.integer(haps$ref_range_id) < 37804, "anchor", "interanchor")
+haps <- haps[order(haps$range_start),] # order haps by ref range start
 
-ggplot(filtered_haps) +
-  geom_point(aes(x = ref_range_id, y = line_name), shape = 15, size = 1, color = "gray") +
-  ggtitle(paste0("Haploplot (Chr ",  chr, ")")) +
-  xlab("Reference Range") +
+hap_g_ranges <- GRanges(
+  seqnames = as.character(haps$chrom),
+  ranges = IRanges(
+    start = haps$range_start,
+    end = haps$range_end,
+    names = paste(paste0("rr", haps$ref_range_id), haps$line_name, sep = "_")
+  ),
+  strand = strand("+"),
+  taxa = haps$line_name,
+  y = haps$y
+)
+seqlengths(hap_g_ranges) <- aggregate(haps$range_end, by = list(haps$chrom), max)$x
+genome(hap_g_ranges) <- "PHG_Maize"
+
+window_g_range <- GRanges(
+  seqnames = as.character(1),
+  ranges = IRanges(
+    start = 1,
+    end = 10000,
+    names = "haploplot_window"
+  ),
+  strand = strand("+")
+)
+
+y_breaks <- seq(levels(haps$line_name))
+y_labels <- levels(haps$line_name)
+
+plot_hap_ranges <- subsetByOverlaps(hap_g_ranges, window_g_range)
+plot_haps <- data.frame(
+  start = start(plot_hap_ranges@ranges),
+  end = end(plot_hap_ranges@ranges),
+  y = plot_hap_ranges@elementMetadata$y,
+  line = plot_hap_ranges@elementMetadata$taxa
+)
+
+ggplot(plot_haps) +
+  geom_rect(aes(xmin = pmax(start, 1), ymin = y - 0.25, xmax = pmin(end, 10000), ymax = y + 0.25), color = "gray") +
+  geom_vline(xintercept = plot_haps$end[plot_haps$end <= 10000] + 0.5, alpha = 0.5) +
+  scale_y_continuous(breaks = y_breaks, labels = y_labels, limits = c(0, length(y_labels) + 1)) +
+  xlab("Position (bp)") +
   ylab("Taxa") +
   theme_minimal() +
-  theme(legend.position = "none", axis.text.x = element_blank())
+  theme(legend.position = "none")
