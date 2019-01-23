0c174b4acfb3136e3e6560542f796c483ab8ffac
Brandon Monier <brandon.monier@gmail.com>
2019-01-23 17:48:15 -0500

    Add conditionals for path parameters

--- R/LoggingSupport.R
+++ R/LoggingSupport.R
@@ -1,8 +1,37 @@
-## Logger
+#' @title Start TASSEL logging information
+#'
+#' @description This function will create a file for storing logging output
+#'    from TASSEL.
+#'
+#' @param fullPath full working path of log file location. If \code{NULL},
+#'    logging file will be added to current working directory.
+#' @param fileName name of logging file. If \code{NULL}, filename will resort
+#'    to "rTASSEL_log".
+#' @param ... additional parameters to be added
+#'
+#' @export
+startLogger <- function(fullPath = NULL, fileName = NULL, ...) {
+    if (is.null(fileName)) {
+        fileName <- "rTASSEL_log"
+    }
 
-startLogger <- function() {
-    file.create("rTASSEL_log")
-    rtlog <- paste0(getwd(), "/rTASSEL_log")
+    if (is.null(fullPath)) {
+        file.create(fileName)
+        rtlog <- file.path(getwd(), fileName)
+    } else {
+        if (grepl(pattern = "~", x = fullPath)) {
+            stop(
+                paste0(
+                    "It seems that you are using a '~' instead of your full",
+                    " home directory path.\n",
+                    "  Consider using: ", Sys.getenv("HOME")
+                )
+            )
+        }
+        file.create(file.path(fullPath, fileName))
+        rtlog <- file.path(fullPath, fileName)
+    }
+
     rJava::.jcall(
         "net.maizegenetics/util/LoggingUtils",
         "V",
@@ -10,5 +39,5 @@ startLogger <- function() {
         rtlog
     )
 
-    message("Logging file created at: ", rtlog)
+    message("TASSEL logging file created at: ", rtlog)
 }
