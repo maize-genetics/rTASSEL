ce0125a369c3e66b1db66bca54fe93a4ab9993f5
Brandon Monier <brandon.monier@gmail.com>
2019-03-04 16:59:15 -0500

    Fix formatting

--- R/GenotypeTableFunctions.R
+++ R/GenotypeTableFunctions.R
@@ -14,99 +14,142 @@
 
 #' @title Wrapper function of TasselGenotypePhenotype class for genotype
 #'    data
-#' 
-#' @description This function is a wrapper for the 
+#'
+#' @description This function is a wrapper for the
 #'    \code{TasselGenotypePhenotype} class. It is used for storing genotype
 #'    information into a class object.
-#' 
+#'
 #' @name readGenotypeTable
 #' @rdname readGenotypeTable
-#' 
+#'
 #' @param path a genotype data path (e.g. \code{*.VCF, *.hmp}, etc.)
-#' 
+#'
 #' @importFrom rJava .jcall
 #' @export
 readGenotypeTable <- function(path) {
-  .tasselObjectConstructor(rJava::.jcall(
-    "net/maizegenetics/dna/snp/ImportUtils",
-    "Lnet/maizegenetics/dna/snp/GenotypeTable;",
-    "read",
-    path
-  )
-  )
+    # if (!file.exists(path)) {
+    #     stop("Cannot open file ", path, ": No such file or directory")
+    # }
+
+    .tasselObjectConstructor(
+        rJava::.jcall(
+            "net/maizegenetics/dna/snp/ImportUtils",
+            "Lnet/maizegenetics/dna/snp/GenotypeTable;",
+            "read",
+            path
+        )
+    )
 }
 
 
 ## Get a GenotypeTable - not exported
 getGenotypeTable <- function(jtsObject) {
-  if(is(jtsObject, "TasselGenotypePhenotype")) {
-    return(jtsObject@jGenotypeTable)
-  }
-  if(!is(jtsObject,"jobjRef")) return(rJava::.jnull())
-  if(jtsObject %instanceof% "net.maizegenetics.dna.snp.GenotypeTable") {
-    return(jtsObject)
-  } else if(jtsObject %instanceof% "net.maizegenetics.phenotype.GenotypePhenotype") {
-    return(jtsObject$genotypeTable())
-  } else {
-    return(rJava::.jnull())
-  }
+    if(is(jtsObject, "TasselGenotypePhenotype")) {
+        return(jtsObject@jGenotypeTable)
+    }
+    if(!is(jtsObject,"jobjRef")) return(rJava::.jnull())
+    if(jtsObject %instanceof% "net.maizegenetics.dna.snp.GenotypeTable") {
+        return(jtsObject)
+    } else if(jtsObject %instanceof% "net.maizegenetics.phenotype.GenotypePhenotype") {
+        return(jtsObject$genotypeTable())
+    } else {
+        return(rJava::.jnull())
+    }
 }
 
 ## Create Summarized Experiment from a TASSEL Genotype Table
 summarizeExperimentFromGenotypeTable <- function(genotypeTable) {
-  jGT <- .getTASSELClass(genotypeTable, "GenotypeTable")
-  sampleDF <- sampleDataFrame(jGT)
-  genomicRangesDF <- genomicRanges(jGT)
-  
-  genoCallIntArray <- rJava::.jcall(
-    "net/maizegenetics/plugindef/GenerateRCode",
-    "[I",
-    "genotypeTableToDosageIntArray",
-    jGT,
-    FALSE
-  )
-  
-  SummarizedExperiment(assays=matrix(genoCallIntArray,length(genomicRangesDF), byrow=TRUE), rowRanges=genomicRangesDF, colData=sampleDF)
+    jGT <- .getTASSELClass(genotypeTable, "GenotypeTable")
+    sampleDF <- sampleDataFrame(jGT)
+    genomicRangesDF <- genomicRanges(jGT)
+
+    genoCallIntArray <- rJava::.jcall(
+        "net/maizegenetics/plugindef/GenerateRCode",
+        "[I",
+        "genotypeTableToDosageIntArray",
+        jGT,
+        FALSE
+    )
+
+    SummarizedExperiment(
+        assays = matrix(
+            genoCallIntArray,
+            length(genomicRangesDF),
+            byrow = TRUE
+        ),
+        rowRanges = genomicRangesDF, colData = sampleDF
+    )
 }
 
 ## Create GWASpoly geno dataframe from SimplifiedExperiment object
-GWASpolyGenoFromSummarizedExperiment <- function(SummarizedExperimentObject){
-  geno <- data.frame(markerName = paste("dummy", 1:length(ranges(SummarizedExperimentObject@rowRanges)), sep = "-"), # dummy name as current summarizeExperimentFromGenotypeTable doesn't keep
-                     chr = seqnames(SummarizedExperimentObject@rowRanges),
-                     pos = start(ranges(SummarizedExperimentObject@rowRanges)),
-                     as.data.frame(SummarizedExperimentObject@assays$data@listData) # same as assay(SummarizedExperimentObject)
-  )
-  colnames(geno)[4:ncol(geno)] <- as.character(SummarizedExperimentObject$Sample)
-  geno
+GWASpolyGenoFromSummarizedExperiment <- function(SummarizedExperimentObject) {
+    geno <- data.frame(
+        markerName = paste(
+            "dummy",
+            1:length(ranges(SummarizedExperimentObject@rowRanges)),
+            sep = "-"
+        ), # dummy name as current summarizeExperimentFromGenotypeTable doesn't keep
+        chr = seqnames(SummarizedExperimentObject@rowRanges),
+        pos = start(ranges(SummarizedExperimentObject@rowRanges)),
+        # same as assay(SummarizedExperimentObject)
+        as.data.frame(SummarizedExperimentObject@assays$data@listData)
+    )
+    colnames(geno)[4:ncol(geno)] <- as.character(SummarizedExperimentObject$Sample)
+    geno
 }
 
 ## Create Summarized Experiment from a TASSEL Genotype Table
 snpMatrixFromGenotypeTable <- function(genotypeTable) {
-  jGT <- .getTASSELClass(genotypeTable, "GenotypeTable")
-  
-  sampleDF <- sampleDataFrame(jGT)
-  genomicRangesDF <- genomicRanges(jGT)
-  
-  genoCallIntArray <- rJava::.jcall(
-    "net/maizegenetics/plugindef/GenerateRCode",
-    "[I",
-    "genotypeTableToDosageIntArray",
-    jGT,
-    FALSE
-  )
-  aMatrix <- matrix(genoCallIntArray,length(genomicRangesDF))
-  colnames(aMatrix) <- sampleDF[["Sample"]]
-  rownames(aMatrix) <- paste0(seqnames(genomicRangesDF),":",ranges(genomicRangesDF))
-  as(aMatrix, "SnpMatrix")
+    jGT <- .getTASSELClass(genotypeTable, "GenotypeTable")
+
+    sampleDF <- sampleDataFrame(jGT)
+    genomicRangesDF <- genomicRanges(jGT)
+
+    genoCallIntArray <- rJava::.jcall(
+        "net/maizegenetics/plugindef/GenerateRCode",
+        "[I",
+        "genotypeTableToDosageIntArray",
+        jGT,
+        FALSE
+    )
+    aMatrix <- matrix(genoCallIntArray,length(genomicRangesDF))
+    colnames(aMatrix) <- sampleDF[["Sample"]]
+    rownames(aMatrix) <- paste0(
+        seqnames(genomicRangesDF),
+        ":",
+        ranges(genomicRangesDF)
+    )
+    as(aMatrix, "SnpMatrix")
 }
 
 ## Create GWASpoly geno dataframe from SimplifiedExperiment object
-GWASpolyGenoFromSummarizedExperiment <- function(SummarizedExperimentObject){
-  geno <- data.frame(markerName = paste("dummy", 1:length(ranges(SummarizedExperimentObject@rowRanges)), sep = "-"), # dummy name as current summarizeExperimentFromGenotypeTable doesn't keep
-                     chr = seqnames(SummarizedExperimentObject@rowRanges),
-                     pos = start(ranges(SummarizedExperimentObject@rowRanges)),
-                     as.data.frame(SummarizedExperimentObject@assays$data@listData) # same as assay(SummarizedExperimentObject)
-  )
-  colnames(geno)[4:ncol(geno)] <- as.character(SummarizedExperimentObject$Sample)
-  geno
+GWASpolyGenoFromSummarizedExperiment <- function(SummarizedExperimentObject) {
+    geno <- data.frame(
+        markerName = paste(
+            "dummy",
+            1:length(ranges(SummarizedExperimentObject@rowRanges)),
+            sep = "-"
+        ), # dummy name as current summarizeExperimentFromGenotypeTable doesn't keep
+        chr = seqnames(SummarizedExperimentObject@rowRanges),
+        pos = start(ranges(SummarizedExperimentObject@rowRanges)),
+        as.data.frame(SummarizedExperimentObject@assays$data@listData) # same as assay(SummarizedExperimentObject)
+    )
+    colnames(geno)[4:ncol(geno)] <- as.character(SummarizedExperimentObject$Sample)
+    geno
 }
+
+
+## Check `readGenotypeTable` for errors
+genoChecker <- function() {
+
+}
+
+
+
+
+
+
+
+
+
+
