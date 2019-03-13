dbcd7be58e0c28fe92cb19f2f585b0d64b6812b8
Brandon Monier <brandon.monier@gmail.com>
2019-03-13 17:01:23 -0400

    Remove old code; reorganize; add tests

--- misc_scripts/btmTests.R
+++ misc_scripts/btmTests.R
@@ -5,7 +5,7 @@
 # Description:   GenomicRanges test for rTASSEL
 # Author:        Brandon Monier
 # Created:       2018-12-04 at 16:46:06
-# Last Modified: 2019-01-23 at 14:36:19
+# Last Modified: 2019-03-13 at 15:07:04
 #--------------------------------------------------------------------
 
 #--------------------------------------------------------------------
@@ -14,72 +14,19 @@
 #    capabilities for rTASSEL in generating GenomicRanges class
 #--------------------------------------------------------------------
 
-# === Create log file and output messages from console ==============
-
-# ## NOTE: For my use only (START AT PREAMBLE) - NOW DEFUNCT
-# setwd("~/Projects/rtassel")
-# path_tassel <- paste0(getwd(),"/inst/java/sTASSEL.jar")
-# rJava::.jinit(parameters = "-Xmx6g")
-#
-# rJava::.jaddClassPath(path_tassel)
-#
-# ## Make rtassel_output file - NOW DEFUNCT
-# rtOut <- paste0(Sys.getenv("HOME"), "/Temporary/rtassel_output")
-# if (!exists(rtOut)) {
-#     system(paste("touch", rtOut))
-# }
-#
-# ## Send TASSEL console output messages to file - NOW DEFUNCT
-# rJava::.jcall(
-#     "net.maizegenetics/util/LoggingUtils",
-#     "V",
-#     "setupLoggingOff"
-# )
-
-
-
 # === Preamble (just load rTASSEL instead) ==========================
 
-# ## Set WD (local use only)
-# setwd("~/Projects/rtassel")
-#
-# ## Load packages
-# library(rJava)
-# library(GenomicRanges)
-# library(stringr)
-# library(SummarizedExperiment)
-# library(snpStats)
-# library(hexbin)
-# library(S4Vectors)
-#
-# ## jinit
+# ## jinit memory test
 # rJava::.jinit(parameters="-Xmx6g")
 # .jcall(.jnew("java/lang/Runtime"), "J", "totalMemory")
 # .jcall(.jnew("java/lang/Runtime"), "J", "maxMemory")
-#
-# ## Add class path
-# path_tassel <- "inst/java/sTASSEL.jar"
-# rJava::.jaddClassPath(path_tassel)
-#
-# ## Which Tassel version
-# tasselVersion <- rJava::.jfield("net/maizegenetics/tassel/TASSELMainFrame","S","version")
-# paste0("Using TASSEL version: ", tasselVersion)
-#
-# ## Source files
-# source("R/AllGenerics.R")
-# source("R/AllClasses.R")
-# source("R/GenotypePhenotypeFunctions.R")
-# source("R/TasselPluginWrappers.R")
-# source("R/GenotypeTableFunctions.R")
-# source("R/PhenotypeFunctions.R")
-# source("R/PositionListFunctions.R")
-# source("R/TaxaListFunctions.R")
-# source("R/TableReportFunctions.R")
-# source("R/PluginSupport.R")
+
+## Start logging file
+rTASSEL::startLogger(fullPath = "/home/bm646/Temporary/")
 
 
 
-# === Tests =========================================================
+# === Get paths =====================================================
 
 ## Genotype file path example
 genoPath <- system.file("extdata", "mdp_genotype.hmp.txt", package = "rTASSEL")
@@ -91,6 +38,10 @@ phenoPath2 <- system.file("extdata", "mdp_phenotype.txt", package = "rTASSEL")
 ## Phenotype data frame example - currently not working
 phenoDF <- read.table(phenoPath, header = TRUE)
 colnames(phenoDF)[1] <- "Taxon"
+
+
+
+# === Read tests ====================================================
 
 ## Read GenotypeTable
 tasGeno <- rTASSEL::readGenotypeTable(genoPath)
@@ -111,8 +62,12 @@ tasGenoPhenoCov <- rTASSEL::readGenotypePhenotype(
     phenoPathDFOrObj = phenoPath2
 )
 
+
+
+# === Assocatiation tests ===========================================
+
 ## Test `assocModelDesign()` - should return BLUE option
-rTASSEL::assocModelDesign(
+assocDF <- rTASSEL::assocModelDesign(
     phenotypeGenotype = tasGenoPhenoCov,
     fixed = list(EarHT, EarDia) ~ location + Q1 + Q2 + Q3 + Taxa,
     kinship = NULL
@@ -133,6 +88,10 @@ rTASSEL::assocModelDesign(
     kinship = "K"
 )
 
+
+
+# ==== Kinship functions ============================================
+
 ## Test `kinshipPlugin()` - return kinship matrix TASSEL object
 ##     calculated from a `TasselGenotypePhenotype` object
 tasKin <- rTASSEL::kinshipPlugin(
@@ -145,14 +104,28 @@ tasKinR <- rTASSEL::kinshipRMatrix(tasKin) ## Convert to R matrix
 tasKinR[1:10, 1:10]                        ## Get subset
 image(tasKinR)                             ## Visualize it
 
+
+
+# === Test for errors ===============================================
+
 ## Test WRONG file path (readGenotypeTable())
-genoWrongPath <- "this/is/so/wrong"
-rTASSEL::readGenotypeTable(path = genoWrongPath)
+# genoWrongPath <- "this/is/so/wrong"
+# rTASSEL::readGenotypeTable(path = genoWrongPath)
 
 ## Test WRONG file path (readPhenotypeTable())
-phenoWrongPath <- "wrong/path"
-rTASSEL::readPhenotypeTable(path = phenoWrongPath)
+# phenoWrongPath <- "wrong/path"
+# rTASSEL::readPhenotypeTable(path = phenoWrongPath)
+
+## Test GLM functionality
+# tasGLM <- rTASSEL:::tasselGLM(tasGenoPheno)
 
 
 
+# === Visualizations for association data ===========================
 
+## Test Manhattan plot functionality
+rTASSEL:::manhattanPlot(
+    assocStats = tasGLM$GLM_Statistics,
+    trait = "dpoll",
+    threshold = 10
+)
