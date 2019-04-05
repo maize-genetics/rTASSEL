a9ecee835672bb77df75de34e36384179aef8cbb
Brandon Monier <brandon.monier@gmail.com>
2019-04-05 14:24:01 -0400

    Make load message more detailed and suggestive

--- R/zzz.R
+++ R/zzz.R
@@ -2,17 +2,19 @@
     ## Initialize Jar
     rJava::.jpackage(pkgname, lib.loc = libname)
     rJava::.jaddClassPath(dir(file.path(getwd(), "inst/java"), full.names = TRUE))
-    rJava::.jcall(
-        "net.maizegenetics/util/LoggingUtils",
-        "V",
-        "setupLoggingOff"
-    )
+    # rJava::.jcall(
+    #     "net.maizegenetics/util/LoggingUtils",
+    #     "V",
+    #     "setupLoggingOff"
+    # )
 }
 
 .onAttach <- function(libname, pkgname){
     msg <- paste0(
-        "Loading rTASSEL - v",
-        utils::packageVersion("rTASSEL")
+        "Welcome to rTASSEL (version ", utils::packageVersion("rTASSEL"), ")", "\n",
+        " \u2022 Consider starting a TASSEL log file (see ?startLogger())", "\n",
+        " \u2022 Additional memory can be added using the following:", "\n",
+        "     options(java.parameters = c(\"-Xmx<>\"))", "\n"
     )
     packageStartupMessage(msg)
 }
