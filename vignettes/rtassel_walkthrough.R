a5768738edf3eb99156ea6ac4c0f01513649bf9b
Brandon Monier <brandon.monier@gmail.com>
2019-04-05 16:37:55 -0400

    Initial commit

---
+++ vignettes/rtassel_walkthrough.R
@@ -0,0 +1,11 @@
+## ----setup, include=FALSE--------------------------------------------------
+knitr::opts_chunk$set(
+    fig.path='figure/graphics-', 
+    cache.path='cache/graphics-', 
+    fig.align='center',
+    external=TRUE,
+    echo=TRUE,
+    warning=FALSE
+    # fig.pos="H"
+)
+
