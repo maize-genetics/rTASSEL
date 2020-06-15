974453ef55bc256fd024cd6bee816e40dc46a71d
Brandon <brandon.monier@gmail.com>
2020-06-15 18:23:34 -0400

    Initial commit

---
+++ tests/testthat/test_manhattan.R
@@ -0,0 +1,112 @@
+# === Tests for Manhattan Plot functions ============================
+
+## Error tests ----
+test_that("Manhattan Plot throws error.", {
+
+    ## Load data ----
+
+    ### Load hapmap data
+    genoPathHMP <- system.file(
+        "extdata",
+        "mdp_genotype.hmp.txt",
+        package = "rTASSEL"
+    )
+
+    ### Read data - need only non missing data!
+    phenoPathFast <- system.file(
+        "extdata",
+        "mdp_traits_nomissing.txt",
+        package = "rTASSEL"
+    )
+
+    ### Create rTASSEL object - use prior TASSEL genotype object
+    tasGenoPhenoFast <- rTASSEL::readGenotypePhenotype(
+        genoPathOrObj = genoPathHMP,
+        phenoPathDFOrObj = phenoPathFast
+    )
+
+
+    ## Fast association ----
+    fastRep <- rTASSEL::assocModelFitter(
+        tasGenoPhenoFast,
+        . ~ .,
+        fastAssociation = TRUE,
+        fitMarkers = TRUE,
+        maxP = 1
+    )
+
+    expect_error(
+        manhattanPlot(
+            assocStats = fastRep$FastAssociation,
+            trait = "dpoll",
+            showSigMarkers = TRUE,
+            showRug = TRUE,
+            colors = c("lightgrey", "red", "blue")
+        ),
+        "Please enter a numeric threshold value."
+    )
+
+})
+
+
+## Equality tests ----
+## Error tests ----
+test_that("Manhattan Plot returns correct plot layers", {
+
+    ## Load data ----
+
+    ### Load hapmap data
+    genoPathHMP <- system.file(
+        "extdata",
+        "mdp_genotype.hmp.txt",
+        package = "rTASSEL"
+    )
+
+    ### Read data - need only non missing data!
+    phenoPathFast <- system.file(
+        "extdata",
+        "mdp_traits_nomissing.txt",
+        package = "rTASSEL"
+    )
+
+    ### Create rTASSEL object - use prior TASSEL genotype object
+    tasGenoPhenoFast <- rTASSEL::readGenotypePhenotype(
+        genoPathOrObj = genoPathHMP,
+        phenoPathDFOrObj = phenoPathFast
+    )
+
+
+    ## Fast association ----
+    fastRep <- rTASSEL::assocModelFitter(
+        tasGenoPhenoFast,
+        . ~ .,
+        fastAssociation = TRUE,
+        fitMarkers = TRUE,
+        maxP = 1
+    )
+
+    ## Plot object ----
+    p <- manhattanPlot(
+        assocStats = fastRep$FastAssociation,
+        trait = "EarHT",
+        showSigMarkers = TRUE,
+        showRug = TRUE,threshold = 6
+    )
+
+    # expect_identical(
+    #     object = class(p$layers[[1]]),
+    #     expected =  c("LayerInstance", "Layer", "ggproto", "gg")
+    # )
+    # expect_identical(
+    #     object = class(p$layers[[1]]$geom[1]),
+    #     expected =  c("GeomPoint")
+    # )
+
+    expect_s3_class(
+        p$layers[[1]],
+        "ggproto"
+    )
+
+})
+
+
