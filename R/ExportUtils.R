c3e2ecc0ab09c07f8d2809db401d6510d87d999b
Brandon <brandon.monier@gmail.com>
2020-11-09 12:29:17 -0500

    Add VCF export methods

--- R/ExportUtils.R
+++ R/ExportUtils.R
@@ -3,7 +3,7 @@
 # Description:   Export utilities for rTASSEL
 # Author:        Brandon Monier
 # Created:       2020-11-09 at 11:53:02
-# Last Modified: 2020-11-09 at 12:12:29
+# Last Modified: 2020-11-09 at 12:27:54
 #--------------------------------------------------------------------
 
 #--------------------------------------------------------------------
@@ -22,10 +22,45 @@
 #' @param file Output file name.
 #' @param format Export file format.
 #'
-#' @export
+#' @importFrom rJava is.jnull
 #'
+#' @export
 exportGenotypeTable <- function(tasObj,
                                 file = "",
                                 format = c("vcf", "hapmap", "plink", "flapjack")) {
-    # TODO get TASSEL methods
+    if (class(tasObj) != "TasselGenotypePhenotype") {
+        stop("`tasObj` must be of class `TasselGenotypePhenotype`")
+    }
+
+    jGenoTable <- getGenotypeTable(tasObj)
+    if (rJava::is.jnull(jGenoTable)) {
+        stop("TASSEL genotype object not found")
+    }
+
+    # Filter type selection
+    format <- match.arg(format)
+    if (missing(format) || !format %in% c("vcf", "hapmap", "plink", "flapjack")) {
+        stop(
+            paste(
+                "Please specify analysis type",
+                "(\"vcf\", \"hapmap\", \"plink\", or \"flapjack\")"
+            )
+        )
+    }
+
+
+    rJC <- rJava::J("net.maizegenetics.dna.snp.ExportUtils")
+
+    if (format == "vcf") {
+        rJC$writeToVCF(
+            tasObj,
+            file,
+            TRUE, # <- keep depth
+            NULL  # <- progress listener
+        )
+    } else {
+        message("Format not yet implemented. Coming soon!")
+    }
 }
+
+
