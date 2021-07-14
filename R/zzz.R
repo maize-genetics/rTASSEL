2cb89de6a9a60fc55cfc6e812c58a9686c6bcac8
Brandon <brandon.monier@gmail.com>
2021-07-14 16:07:20 -0400

    Remove jars

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,13 @@
-.onLoad <- function(libname, pkgname) {
-    ## Initialize Jar
-    rJava::.jpackage(pkgname, lib.loc = libname)
-    rJava::.jaddClassPath(dir(file.path(getwd(), "inst/java"), full.names = TRUE))
-    # rJava::.jcall(
-    #     "net.maizegenetics/util/LoggingUtils",
-    #     "V",
-    #     "setupLoggingOff"
-    # )
-}
+# .onLoad <- function(libname, pkgname) {
+#     ## Initialize Jar
+#     rJava::.jpackage(pkgname, lib.loc = libname)
+#     rJava::.jaddClassPath(dir(file.path(getwd(), "inst/java"), full.names = TRUE))
+#     # rJava::.jcall(
+#     #     "net.maizegenetics/util/LoggingUtils",
+#     #     "V",
+#     #     "setupLoggingOff"
+#     # )
+# }
 
 .onAttach <- function(libname, pkgname){
 
