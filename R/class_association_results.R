fd4229ba9f6434edbde076e5f5f7d5352c9aa196
Brandon <brandon.monier@gmail.com>
2023-06-29 12:13:46 -0400

    Add show method

--- R/class_association_results.R
+++ R/class_association_results.R
@@ -1,11 +1,11 @@
 ## ----
 #' @title AssociationResults Class
 #'
-#' @description 
+#' @description
 #' Class \code{AssociationResults} defines a \code{rTASSEL}
 #' Class for storing TASSEL 5 and general-purpose association results
 #' from *WAS studies.
-#' 
+#'
 #' @slot results A list of \code{data.frame} objects containing summary results
 #' @slot traits A vector of type \code{character} containing the trait IDs
 #'    modeled.
@@ -22,18 +22,18 @@ setClass(
 )
 
 #' @title AssociationResults validation
-#' 
+#'
 #' @name AssociationResults-validity
-#' 
+#'
 #' @description
 #' Checks if all elements of list are \code{data.frame} objects
-#' 
+#'
 #' @param object A \code{AssociationResults} object
 setValidity("AssociationResults", function(object) {
     errors <- character()
-    
+
     dfCheck <- vapply(object@results, is.data.frame, logical(1))
-    
+
     if (!all(dfCheck)) {
         invalidIndices <- which(!dfCheck)
         if (length(invalidIndices) == 1) {
@@ -48,13 +48,90 @@ setValidity("AssociationResults", function(object) {
         msg <- paste0("Invalid list: ", invalidElements)
         errors <- c(errors, msg)
     }
-    
-    
+
     if (length(errors) == 0) {
         return(TRUE)
     } else {
         return(errors)
     }
 })
+
+
+## ----
+#' @title Show methods for AssociationResults objects
+#'
+#' @description
+#' Prints outs summary information from \code{AssociationResults} objects
+#'
+#' @param object a \code{\linkS4class{AssociationResults}} object
+#'
+#' @docType methods
+#' @name show
+#' @rdname show
+#' @aliases show, AssociationResults-method
+setMethod(
+    f = "show",
+    signature = "AssociationResults",
+    definition = function(object) {
+        # local parameters
+        maxTraitsToPrint <- 5
+        indentStyle <- "  *"
+
+        # header text
+        titleMsg <- paste(
+            "AssociationResults object with", length(object@results),
+            "reports and", length(object@traits), "mapped traits \n"
+        )
+        cat(titleMsg)
+
+        # results text
+        cat("Results:\n")
+        dims <- vapply(object@results, dim, numeric(2))
+        for (i in seq_along(object@results)) {
+            dfName  <- names(object@results[i])
+            currDim <- paste0("(", paste(dims[, dfName], collapse = ", "), ")")
+            cat(indentStyle, dfName, currDim, "\n")
+        }
+
+        # trait text
+        cat("Traits:\n")
+        if (length(object@traits) <= maxTraitsToPrint) {
+            msgTraits <- paste(object@traits, collapse = ", ")
+        } else {
+            remTraits <- length(object@traits) - maxTraitsToPrint
+
+            for (i in seq_len(maxTraitsToPrint)) {
+                cat(indentStyle, object@traits[i], "\n")
+            }
+            cat("    ...\n")
+            cat(paste("  (with", remTraits, "more values)\n"))
+        }
+    }
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- tests/testthat/test_class_association_results.R
+++ tests/testthat/test_class_association_results.R
@@ -1,6 +1,7 @@
 # === Tests for association class ===================================
 
-test_that("AssociationResults methods return correct data", {
+test_that("AssociationResults methods return correct data and exceptions", {
+    # Test instantiation
     testClass <- methods::new(
         "AssociationResults",
         results = list(),
@@ -31,5 +32,28 @@ test_that("AssociationResults methods return correct data", {
         ),
         regexp = "should be or extend class \"character\""
     )
+
+    # Test show method
+    classShowFew <- capture_output(
+        show(
+            methods::new(
+                "AssociationResults",
+                results = list("td_1" = iris, "td_2" = mtcars),
+                traits = paste0("trait_", 1:3)
+            )
+        )
+    )
+    classShowMany <- capture_output(
+        show(
+            methods::new(
+                "AssociationResults",
+                results = list("td_1" = iris, "td_2" = mtcars),
+                traits = paste0("trait_", 1:10)
+            )
+        )
+    )
+
+    expect_true(grepl("AssociationResults object with 2", classShowFew))
+    expect_true(grepl("(with 5 more values)", classShowMany))
 })
 
