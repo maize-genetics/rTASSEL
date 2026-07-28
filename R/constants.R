8e8862c0be2cbe62fd5267e4b346eebf29bc070f
Brandon Monier <bm646@cornell.edu>
2026-07-28 12:01:48 -0500

    Update Maven specs

--- R/constants.R
+++ R/constants.R
@@ -17,7 +17,8 @@ TASSEL_JVM <- list(
     "REF_PROBABILITY_BUILDER"  = "net.maizegenetics.dna.snp.score.ReferenceProbabilityBuilder",
     "STEPWISE_FITTER"          = "net.maizegenetics.analysis.modelfitter.StepwiseOLSModelFitter",
     "STEPWISE_PLUGIN"          = "net.maizegenetics.analysis.modelfitter.StepwiseOLSModelFitterPlugin",
-    "TAXA_LIST_BUILDER"        = "net.maizegenetics.taxa.TaxaListBuilder"
+    "TAXA_LIST_BUILDER"        = "net.maizegenetics.taxa.TaxaListBuilder",
+    "VERSIONS"                 = "net.maizegenetics.tassel.TasselVersions"
 )
 
 
@@ -43,10 +44,33 @@ ANSI <- list(
 TASSEL_MAVEN <- list(
     "BASE_URL"      = "https://repo1.maven.org/maven2",
     "GROUP_PATH"    = "net/maizegenetics",
+    "GROUP_ID"      = "net.maizegenetics",
     "ARTIFACT_ID"   = "tassel",
     "VERSION"       = "5.2.96",
     "CLASSIFIER"    = "jar-with-dependencies",
-    "SHA1_CHECKSUM" = "9320966721a12741da2a60f02fd3830639058d63"
+    "SHA1_CHECKSUM" = "9320966721a12741da2a60f02fd3830639058d63",
+
+    # TASSEL's own build declares SciJava (for 'cisd:jhdf5') and JBoss (for
+    # 'openchart', a transitive of 'forester') in addition to Maven Central.
+    # Neither artifact is resolvable from Central alone.
+    "REPOS" = c(
+        "central" = "https://repo1.maven.org/maven2",
+        "scijava" = "https://maven.scijava.org/content/repositories/public",
+        "jboss"   = "https://repository.jboss.org/maven2"
+    )
+)
+
+
+## ----
+# TASSEL Update Check Constants
+#
+# @description
+# Settings governing the Maven Central version check performed when the
+# package is attached.
+TASSEL_UPDATE <- list(
+    "CACHE_FILE"   = "version-check.rds",
+    "MAX_AGE_SECS" = 24 * 60 * 60,
+    "TIMEOUT_SECS" = 3L
 )
 
 
