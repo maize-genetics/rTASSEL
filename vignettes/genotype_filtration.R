d9aaf2f49e8f48026373d2033de1bb87a1882299
Brandon Monier <bm646@cornell.edu>
2026-09-07 12:12:53 -0500

    Initial dplyr draft

--- vignettes/genotype_filtration.R
+++ vignettes/genotype_filtration.R
@@ -28,44 +28,37 @@ myRealGT |> filterSites(maf >= 0.05)
 
 
 ## ----eval=FALSE---------------------------------------------------------------
-# myGT[taxa("B73", "Mo17"), ]           # myGT |> selectTaxa("B73", "Mo17")
-# myGT[taxaWhere(notMissing >= 0.8), ]  # myGT |> filterTaxa(notMissing >= 0.8)
-# myGT[, sites(1:100)]                  # myGT |> sliceSites(1:100)
-# myGT[, !sites(1:10)]                  # myGT |> sliceSites(-(1:10))
-# myGT[, siteIds(myMarkers)]            # myGT |> selectSites(all_of(myMarkers))
-# myGT[, chrom("1", "5")]               # myGT |> filterSites(chrom %in% c("1", "5"))
-# myGT[, region("1", 1e6, 2e6)]         # myGT |> filterSites(chrom == "1", pos >= 1e6, pos <= 2e6)
-# myGT[, region(gr)]                    # myGT |> filterSites(overlaps(gr))
-# myGT[, sitesWhere(maf >= 0.05 & !isIndel)]
-#                                       # myGT |> filterSites(maf >= 0.05, !isIndel)
-
-
-## ----eval=FALSE---------------------------------------------------------------
-# myGT |> filterSites(overlaps(gr), maf >= 0.05)
-
-
-## ----eval=FALSE---------------------------------------------------------------
 # myGT
 
 
 ## ----eval=FALSE---------------------------------------------------------------
 # myGT[, sitesWhere(maf >= 0.3)]
+# 
+# myGT |> filterSites(maf >= 0.3)
 
 
 ## ----eval=FALSE---------------------------------------------------------------
 # myGT[, sitesWhere(maf >= 0.2 & maf <= 0.3)]
+# 
+# myGT |> filterSites(maf >= 0.2, maf <= 0.3)
 
 
 ## ----eval=FALSE---------------------------------------------------------------
 # myGT[, sitesWhere(alleleCount >= 10)]
+# 
+# myGT |> filterSites(alleleCount >= 10)
 
 
 ## ----eval=FALSE---------------------------------------------------------------
 # myGT[, sitesWhere(het <= 0.1)]
+# 
+# myGT |> filterSites(het <= 0.1)
 
 
 ## ----eval=FALSE---------------------------------------------------------------
 # myGT[, sites(2:4)]
+# 
+# myGT |> sliceSites(2:4)
 
 
 ## ----eval=FALSE---------------------------------------------------------------
@@ -74,14 +67,22 @@ myRealGT |> filterSites(maf >= 0.05)
 
 ## ----eval=FALSE---------------------------------------------------------------
 # myGT[, chrom("1")]
+# 
+# myGT |> filterSites(chrom == "1")
 
 
 ## ----eval=FALSE---------------------------------------------------------------
 # myGT[, region("1", 250, 542)]
+# 
+# myGT |> filterSites(chrom == "1", pos >= 250, pos <= 542)
 
 
 ## ----eval=TRUE, echo=TRUE-----------------------------------------------------
 myRealGT[, region("2", 20e6, 30e6)]
+
+
+## ----eval=TRUE, echo=TRUE-----------------------------------------------------
+myRealGT |> filterSites(chrom == "2", pos >= 20e6, pos <= 30e6)
 
 
 ## ----eval=FALSE---------------------------------------------------------------
@@ -90,6 +91,8 @@ myRealGT[, region("2", 20e6, 30e6)]
 
 ## ----eval=FALSE---------------------------------------------------------------
 # myGT[, region(gr)]
+# 
+# myGT |> filterSites(overlaps(gr))
 
 
 ## ----eval=TRUE, echo=TRUE-----------------------------------------------------
@@ -105,7 +108,15 @@ myRealGT[, region(gr)]
 
 
 ## ----eval=TRUE, echo=TRUE-----------------------------------------------------
+myRealGT |> filterSites(overlaps(gr))
+
+
+## ----eval=TRUE, echo=TRUE-----------------------------------------------------
 myRealGT[, sitesWhere(overlaps(gr) & maf >= 0.05)]
+
+
+## ----eval=TRUE, echo=TRUE-----------------------------------------------------
+myRealGT |> filterSites(overlaps(gr), maf >= 0.05)
 
 
 ## $ cat my_ranges.bed
@@ -116,6 +127,8 @@ myRealGT[, sitesWhere(overlaps(gr) & maf >= 0.05)]
 
 ## ----eval=FALSE---------------------------------------------------------------
 # myGT[, region(rtracklayer::import("my_ranges.bed"))]
+# 
+# myGT |> filterSites(overlaps(rtracklayer::import("my_ranges.bed")))
 
 
 ## $ cat my_chr_pos.tsv
@@ -134,10 +147,16 @@ myRealGT[, sitesWhere(overlaps(gr) & maf >= 0.05)]
 # )
 # 
 # myGT[, region(gr)]
+# 
+# myGT |> filterSites(overlaps(gr))
 
 
 ## ----eval=TRUE, echo=TRUE-----------------------------------------------------
 myRealGT[, siteIds("PZB00859.1", "PZA01271.1")]
+
+
+## ----eval=TRUE, echo=TRUE-----------------------------------------------------
+myRealGT |> selectSites("PZB00859.1", "PZA01271.1")
 
 
 ## ----eval=TRUE, echo=TRUE-----------------------------------------------------
@@ -147,34 +166,65 @@ myRealGT[, myMarkers]
 
 
 ## ----eval=TRUE, echo=TRUE-----------------------------------------------------
+myRealGT |> selectSites(all_of(myMarkers))
+
+
+## ----eval=TRUE, echo=TRUE-----------------------------------------------------
+myRealGT |> selectSites(starts_with("PZA00"))
+
+
+## ----eval=TRUE, echo=TRUE-----------------------------------------------------
 myRealGT[, !sites(1:10)]
+
+
+## ----eval=TRUE, echo=TRUE-----------------------------------------------------
+myRealGT |> sliceSites(-(1:10))
+
+
+## ----eval=FALSE---------------------------------------------------------------
+# myGT[, !siteIds("rs1", "rs2")]        # myGT |> selectSites(-any_of(c("rs1", "rs2")))
+# myGT[, !sitesWhere(isIndel)]          # myGT |> filterSites(!isIndel)
 
 
 ## ----eval=FALSE---------------------------------------------------------------
 # myGT[taxaWhere(notMissing >= 1.0), ]
+# 
+# myGT |> filterTaxa(notMissing >= 1.0)
 
 
 ## ----eval=FALSE---------------------------------------------------------------
 # myGT[taxaWhere(het <= 0.0), ]
+# 
+# myGT |> filterTaxa(het <= 0.0)
 
 
 ## ----eval=FALSE---------------------------------------------------------------
 # myGT[taxa("B73", "B97", "Ky21"), ]
+# 
+# myGT |> selectTaxa("B73", "B97", "Ky21")
 
 
 ## ----eval=FALSE---------------------------------------------------------------
 # myFavTaxa <- c("B73", "B97", "Ky21")
 # 
 # myGT[taxa(myFavTaxa), ]
+# 
+# myGT |> selectTaxa(all_of(myFavTaxa))
 
 
 ## ----eval=TRUE, echo=TRUE-----------------------------------------------------
 myRealGT[c("33-16", "38-11", "4226"), ]
 
 
+## ----eval=TRUE, echo=TRUE-----------------------------------------------------
+myRealGT |> selectTaxa(c("33-16", "38-11", "4226"))
+
+
 ## ----eval=FALSE---------------------------------------------------------------
 # # All taxa whose ID starts with "B" or "Ky"
 # myGT[taxaWhere(grepl("^B|^Ky", taxaId)), ]
+# 
+# myGT |> filterTaxa(grepl("^B|^Ky", taxaId))
 
 
 ## ----eval=TRUE, echo=TRUE-----------------------------------------------------
@@ -182,11 +232,27 @@ myRealGT[taxaWhere(startsWith(taxaId, "CML")), ]
 
 
 ## ----eval=TRUE, echo=TRUE-----------------------------------------------------
+myRealGT |> filterTaxa(startsWith(taxaId, "CML"))
+
+
+## ----eval=TRUE, echo=TRUE-----------------------------------------------------
+myRealGT |> selectTaxa(starts_with("CML"))
+
+
+## ----eval=TRUE, echo=TRUE-----------------------------------------------------
 myRealGT[taxaWhere(startsWith(taxaId, "CML") & notMissing >= 0.9), ]
+
+
+## ----eval=TRUE, echo=TRUE-----------------------------------------------------
+myRealGT |> filterTaxa(startsWith(taxaId, "CML"), notMissing >= 0.9)
 
 
 ## ----eval=FALSE---------------------------------------------------------------
 # myGT[taxaWhere(grepl("^B|^Ky", taxaId)), sites(2:4)]
+# 
+# myGT |>
+#     filterTaxa(grepl("^B|^Ky", taxaId)) |>
+#     sliceSites(2:4)
 
 
 ## ----eval=TRUE, echo=TRUE-----------------------------------------------------
@@ -203,7 +269,20 @@ myRealGT |>
 
 
 ## ----eval=FALSE---------------------------------------------------------------
+# myRealGT[taxaWhere(notMissing >= 0.8), ] |>
+#     filterSites(maf >= 0.05)
+
+
+## ----eval=FALSE---------------------------------------------------------------
 # myRealGT[taxaWhere(notMissing >= 0.8), sitesWhere(maf >= 0.05)] |>
+#     exportGenotypeTable(
+#         file   = "my_filtered_gt.vcf",
+#         format = "vcf"
+#     )
+# 
+# myRealGT |>
+#     filterTaxa(notMissing >= 0.8) |>
+#     filterSites(maf >= 0.05) |>
 #     exportGenotypeTable(
 #         file   = "my_filtered_gt.vcf",
 #         format = "vcf"
@@ -230,6 +309,8 @@ myRealGT |> removeMinorSNPStates()
 
 ## ----eval=FALSE---------------------------------------------------------------
 # myRealGT[, sitesWhere(maf >= 0.05)] |> removeMinorSNPStates()
+# 
+# myRealGT |> filterSites(maf >= 0.05) |> removeMinorSNPStates()
 
 
 ## ----eval=FALSE---------------------------------------------------------------
@@ -240,8 +321,11 @@ myRealGT |> removeMinorSNPStates()
 #         siteMinAlleleFreq = 0.05
 #     )
 # 
-# # After
+# # After (brackets)
 # myGT[, sitesWhere(alleleCount >= 150 & maf >= 0.05)]
+# 
+# # After (verbs)
+# myGT |> filterSites(alleleCount >= 150, maf >= 0.05)
 
 
 ## ----eval=FALSE---------------------------------------------------------------
@@ -250,6 +334,11 @@ myRealGT |> removeMinorSNPStates()
 #     filterGenotypeTableTaxa(minNotMissing = 0.8) |>
 #     filterGenotypeTableSites(siteMinAlleleFreq = 0.05)
 # 
-# # After
+# # After (brackets)
 # myGT[taxaWhere(notMissing >= 0.8), sitesWhere(maf >= 0.05)]
+# 
+# # After (verbs)
+# myGT |>
+#     filterTaxa(notMissing >= 0.8) |>
+#     filterSites(maf >= 0.05)
 
